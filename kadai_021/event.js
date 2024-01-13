43a9180546442bc7eb64031d53fdf7f983eb69cc
hori-hayato <152762969+hori-hayato@users.noreply.github.com>
2024-01-13 22:59:37 +0900

    kadai_021課題提出

---
+++ kadai_021/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+setTimeout(() => {
+    const text = document.getElementById('text');
+      text.textContent = 'ボタンをクリックしました';
+  }, 2000);

058fb8321726eb9187ef552e4b5db87f3a5a3bc7
hori-hayato <152762969+hori-hayato@users.noreply.github.com>
2024-01-14 13:13:14 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-setTimeout(() => {
-    const text = document.getElementById('text');
-      text.textContent = 'ボタンをクリックしました';
-  }, 2000);
